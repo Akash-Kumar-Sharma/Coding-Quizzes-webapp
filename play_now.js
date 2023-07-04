8ffd2b53a6cb00d77b17b85a55663bd07541f29d
Akash Sharma <akashkumarsharma2407@gmail.com>
2023-07-04 17:31:22 +0530

    Design part is complete

---
+++ play_now.js
@@ -0,0 +1,50 @@
+const questions = [ 
+    {
+        question: "What is the smallest header in HTML by default?",
+        answer: [
+                {text: "5" ,correct: false},
+                {text: "3" ,correct: false},
+                {text: "1" ,correct: false},
+                {text: "6" ,correct: true},  
+        ]
+    },
+
+    {
+        question: "How to create an ordered list in HTML?",
+        answer: [
+                {text: "<ol>" ,correct: true},
+                {text: "<ul>" ,correct: false},
+                {text: "<li>" ,correct: false},
+                {text: "<b>" ,correct: false},  
+        ]
+    },
+    {
+        question: "HTML files are saved by default with the extension?",
+        answer: [
+                {text: ".h" ,correct: false},
+                {text: ".hml" ,correct: false},
+                {text: ".html" ,correct: true},
+                {text: ".ht" ,correct: false},  
+        ]
+    },
+
+    {
+        question: "We enclose HTML tags within?",
+        answer: [
+                {text: "{}" ,correct: false},
+                {text: "<>" ,correct: true},
+                {text: "!!" ,correct: false},
+                {text: "/" ,correct: false},  
+        ]
+    },
+    {
+        question: "How are quotations defined in HTML?",
+        answer: [
+                {text: "<quote>" ,correct: false},
+                {text: "block" ,correct: false},
+                {text: "blockquote" ,correct: false},
+                {text: "none of above" ,correct: true},  
+        ]
+    }
+    
+]
